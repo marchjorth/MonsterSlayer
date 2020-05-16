4317e60881a5dc314617063a77e335173a9b37f6
Marc Hjorth <mah@clienti.dk>
2020-05-16 12:23:50 +0200

    Vetur extension Format Document with Tab Size 4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
-<div id="app">
+    <div id="app">
         <div class="container">
             <h1>{{ title }}</h1>
-            <hr>
+            <hr />
 
             <div class="players">
                 <div class="you">
@@ -24,12 +24,21 @@
 
             <div class="controls">
                 <button class="btn btn__attack" @click="attack">Attack</button>
-                <button class="btn btn__attack--special" :disabled="extras.specialCooldown == true"
-                    @click="specialAttack">Special attack</button>
+                <button
+                    class="btn btn__attack--special"
+                    :disabled="extras.specialCooldown == true"
+                    @click="specialAttack"
+                >Special attack</button>
                 <button class="btn btn__heal" @click="heal">Heal</button>
-                <button class="btn btn__give-up" @click="surrenderConfirmation">{{ extras.surrenderText }}</button>
-                <button v-if="extras.surrender == true" class="btn btn__give-up btn__give-up--surrender" @click="surrender">Yes</button>
-
+                <button
+                    class="btn btn__give-up"
+                    @click="surrenderConfirmation"
+                >{{ extras.surrenderText }}</button>
+                <button
+                    v-if="extras.surrender == true"
+                    class="btn btn__give-up btn__give-up--surrender"
+                    @click="surrender"
+                >Yes</button>
             </div>
 
             <div class="status">
@@ -38,8 +47,7 @@
                         <li v-for="m in message" :key="m.id">{{ m }}</li>
                     </template>
                 </ul>
-            </div> 
-
+            </div>
         </div>
 
         <section class="modal" id="modal">
@@ -48,65 +56,66 @@
                 <button @click="restart()" class="btn btn__restart">{{ extras.endButtonText }}</button>
             </div>
         </section>
-
     </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-        title: "Monster Slayer",
-        user: {
-            health: 100
-        },
-        monster: {
-            health: 100
-        },
-        extras: {
-            specialCooldown: false,
-            moves: 0,
-            surrender: false,
-            surrenderText: 'Surrender',
-            result: '',
-            endButtonText: ''
-        },
-        messages: []
-    }
-  },
-  methods: {
-        attack: function () {
+    name: "app",
+    data() {
+        return {
+            title: "Monster Slayer",
+            user: {
+                health: 100
+            },
+            monster: {
+                health: 100
+            },
+            extras: {
+                specialCooldown: false,
+                moves: 0,
+                surrender: false,
+                surrenderText: "Surrender",
+                result: "",
+                endButtonText: ""
+            },
+            messages: []
+        };
+    },
+    methods: {
+        attack: function() {
             var userDamage = this.roll(10, 1);
             this.monster.health -= userDamage;
 
             var monsterDamage = this.roll(10, 1);
             this.user.health -= monsterDamage;
 
-            this.roundStatus('dmg', userDamage, monsterDamage);
+            this.roundStatus("dmg", userDamage, monsterDamage);
 
             this.extras.moves++;
             this.coolDown();
         },
-        win: function () {
-          var modal = document.getElementById('modal');
+        win: function() {
+            var modal = document.getElementById("modal");
             if (this.user.health <= 0) {
-                this.extras.result = 'You lost..';
-                this.extras.endButtonText = 'Try again';
-                modal.classList.add('active');
-                
+                this.extras.result = "You lost..";
+                this.extras.endButtonText = "Try again";
+                modal.classList.add("active");
             } else if (this.monster.health <= 0) {
-                this.extras.result = 'You slayed the monster!'
-                this.extras.endButtonText = 'Play again'
-                modal.classList.add('active');
+                this.extras.result = "You slayed the monster!";
+                this.extras.endButtonText = "Play again";
+                modal.classList.add("active");
             }
         },
-        roll: function (amplifier, minAmount) {
-            var randomNumber = Math.max(Math.floor(Math.random() * amplifier) + 1, minAmount);
+        roll: function(amplifier, minAmount) {
+            var randomNumber = Math.max(
+                Math.floor(Math.random() * amplifier) + 1,
+                minAmount
+            );
             return randomNumber;
         },
-        heal: function () {
-            if (this.user.health !== 100) {
+        heal: function() {
+            if (this.user.health != 100) {
                 var healAmount = this.roll(10, 3);
                 this.user.health += healAmount;
 
@@ -117,14 +126,14 @@ export default {
                 this.extras.heal = true;
                 this.coolDown();
 
-                this.roundStatus('heal', healAmount, monsterDamage);
+                this.roundStatus("heal", healAmount, monsterDamage);
             }
         },
-        surrenderConfirmation: function () {
+        surrenderConfirmation: function() {
             this.extras.surrender = true;
-            this.extras.surrenderText = 'Are you sure?'
+            this.extras.surrenderText = "Are you sure?";
         },
-        surrender: function () {
+        surrender: function() {
             this.monster.health = 100;
             this.user.health = 100;
 
@@ -132,9 +141,9 @@ export default {
             this.extras.moves = 0;
             this.messages = [];
             this.extras.surrender = false;
-            this.extras.surrenderText = 'Surrender'
+            this.extras.surrenderText = "Surrender";
         },
-        specialAttack: function () {
+        specialAttack: function() {
             var userDamage = this.roll(20, 4);
             this.monster.health -= userDamage;
 
@@ -144,46 +153,62 @@ export default {
             this.extras.specialCooldown = true;
             this.extras.moves = 0;
 
-            this.roundStatus('dmg', userDamage, monsterDamage);
+            this.roundStatus("dmg", userDamage, monsterDamage);
         },
-        coolDown: function () {
+        coolDown: function() {
             if (this.extras.moves % 3 === 0) {
                 this.extras.specialCooldown = false;
             }
         },
-        roundStatus: function (type, userMessage, monsterMessage) {
-            if (String(type).toLowerCase() == 'heal') {
-                this.messages.push({
-                    message: 'You healed for ' + userMessage + ' hitpoints this round'
-                }, {
-                        message: 'Monster did ' + monsterMessage + ' damage this round'
-                    });
+        roundStatus: function(type, userMessage, monsterMessage) {
+            if (String(type).toLowerCase() == "heal") {
+                this.messages.push(
+                    {
+                        message:
+                            "You healed for " +
+                            userMessage +
+                            " hitpoints this round"
+                    },
+                    {
+                        message:
+                            "Monster did " +
+                            monsterMessage +
+                            " damage this round"
+                    }
+                );
             } else {
-                this.messages.push({
-                    message: 'You did ' + userMessage + ' damage this round'
-                }, {
-                        message: 'Monster did ' + monsterMessage + ' damage this round'
-                    });
+                this.messages.push(
+                    {
+                        message: "You did " + userMessage + " damage this round"
+                    },
+                    {
+                        message:
+                            "Monster did " +
+                            monsterMessage +
+                            " damage this round"
+                    }
+                );
             }
 
-            this.win()
+            this.win();
         },
         restart: function() {
-            var modal = document.querySelector('#modal');
-            modal.classList.remove('active');
+            var modal = document.querySelector("#modal");
+            modal.classList.remove("active");
             this.surrender();
         }
-      }
-}
+    }
+};
 </script>
 
 <style lang="scss">
 * {
     box-sizing: border-box;
-    font-family: 'Open Sans', sans-serif; 
+    font-family: "Open Sans", sans-serif;
 }
 
-html, body {
+html,
+body {
     padding: 0;
     margin: 0;
 }
@@ -209,7 +234,6 @@ h1 {
 hr {
     max-width: 10%;
     margin: 15px auto 15px;
-
 }
 .players {
     margin-top: 60px;
@@ -237,14 +261,14 @@ hr {
     border: 2px solid black;
     font-weight: bold;
     font-size: 14px;
-    transition: background-color .3s ease;
+    transition: background-color 0.3s ease;
 
     &:hover {
         cursor: pointer;
         background-color: #000;
-        color: #FFF;
+        color: #fff;
     }
-    
+
     &__attack {
         border-color: #466db3;
 
@@ -287,12 +311,12 @@ hr {
     }
 
     &__restart {
-        border-color: #FFF;
-        color: #FFF;
+        border-color: #fff;
+        color: #fff;
         background-color: transparent;
 
         &:hover {
-            background-color: #FFF;
+            background-color: #fff;
             color: #000;
         }
     }
@@ -312,7 +336,7 @@ li {
     span {
         position: absolute;
         left: 50%;
-        transform: translateX(-50%)
+        transform: translateX(-50%);
     }
 }
 
@@ -328,7 +352,6 @@ li {
         display: flex;
         flex-direction: column-reverse;
         padding-left: 0;
-
 
         li {
             padding: 4px 10px;
@@ -354,14 +377,13 @@ li {
     }
 }
 
-
 .modal {
     position: fixed;
-    top:0;
-    left:0;
-    height:100%;
+    top: 0;
+    left: 0;
+    height: 100%;
     width: 100%;
-    z-index:101;
+    z-index: 101;
     background-color: rgba(0, 0, 0, 0.9);
     visibility: hidden;
     opacity: 0;
